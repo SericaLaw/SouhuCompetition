2535d4bbae85b2c69becde9dc50486b356624ea6
fallinglight <213160418@seu.edu.cn>
2019-05-04 18:08:43 +0800

    give up!!

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import argparse
 from src.entity_extractor import EntityExtractor
-from src.doc_encoder import stackpassage, PassageEncoder, train
+from src.doc_encoder import stackpassage, PassageEncoder, train, load_model
 from src.utils import load_data
+import torch
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model_path", default="./model")
+parser.add_argument("--model_path", default="./src/model")
 parser.add_argument("--num_epoch",default=10,type=int)
 parser.add_argument("--lr", default=1e-2)
 parser.add_argument("--eval", action="store_true")
@@ -24,12 +25,14 @@ def main():
     extractor = EntityExtractor(NUM_CANDIDATE)
     data = load_data('./src/data/test.txt')
     data = [data[i:i + BATCH_SIZE] for i in range(0, len(data), BATCH_SIZE)]
+    model, cur_epoch = load_model(MODEL_PATH, DIMENSION)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    print("start training")
     for idx, items in enumerate(data):
         passages = extractor.get_passages(items)
         dataloader = stackpassage(passages)
-        model = PassageEncoder(DIMENSION,DIMENSION)
-        train(dataloader,model)
-
+        train(dataloader,model,optimizer,save_path=MODEL_PATH, cur_epoch=cur_epoch)
+        cur_epoch += 1
 
 
 if __name__ == "__main__":
