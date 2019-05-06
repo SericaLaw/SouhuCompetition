2b0f4e9953ac2b40bc6684c81de2a8791200edc3
Serica <943914044@qq.com>
2019-05-06 16:48:31 +0800

    add f1score

--- src/utils.py
+++ src/utils.py
@@ -30,3 +30,30 @@ def similarity(passage_encode, entity):
 
 
 
+def F1Score(truth_prediction_list):
+    '''
+    [{passage: [grand truth list], y_hat: [prediction list]}, ... ]
+    :return: F1 Score
+    '''
+
+    true_possitive = false_positive = false_negative = 0
+    for d in truth_prediction_list:
+        truth = d['passage']
+        y_hat = d['y_hat']
+        for t in truth:
+            if t in y_hat:
+                true_possitive += 1
+            else:
+                false_negative += 1
+        for h in y_hat:
+            if h not in truth:
+                false_positive += 1
+    precision = true_possitive / (true_possitive + false_positive)
+    recall = true_possitive / (true_possitive + false_negative)
+    return 2 * precision * recall / (precision + recall)
+
+
+if __name__ == '__main__':
+    test = [{'passage': ['a', 'b', 'c'], 'y_hat': ['a', 'b', 'c']},
+            {'passage': ['d', 'e', 'f'], 'y_hat': ['d', 'e']}]
+    print(F1Score(test))
